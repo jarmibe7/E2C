809fd86e14261a1af2f59aa1bb3c340a5ae610ae
Ayush Gaggar <gaggarayush@gmail.com>
2026-01-14 17:46:10 -0600

    config for push

--- src/main.py
+++ src/main.py
@@ -36,7 +36,7 @@ def main():
     print('*** STARTING ***\n')
     # Load config, make run path, and choose torch device
     # ---------- CONFIG HERE ----------
-    config_name = 'rssm_reacher_active_random_v0'   # <--- CHANGE CONFIG HERE
+    config_name = 'rssm_push_active_v0'   # <--- CHANGE CONFIG HERE
     with open(CONFIG_PATH / f'{config_name}.yaml', "r") as f:
         config = yaml.safe_load(f)
     config['config_name'] = config_name
