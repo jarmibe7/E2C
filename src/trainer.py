5cd04f663ddd91d57a274da1168a63fbfb439e19
Ayush Gaggar <gaggarayush@gmail.com>
2026-01-26 03:47:33 -0600

    saving chkpt every 50 epochs

--- src/trainer.py
+++ src/trainer.py
@@ -124,14 +124,13 @@ class BaseTrainer():
         # self.evaluator.eval(run_path)
         self.evaluator.visualize_planner(self, run_path, max_steps=50, closed_loop=True)
 
-    def save(self, config_save, run_path):
+    def save(self, config_save, run_path, model_name='model.pt'):
         """
         Save model, policy, and config to run directory
         """
         self.plotter.save(run_path)
 
         # Save model
-        model_name = 'model.pt'
         try:
             filepath = run_path / model_name
             print(f'Saved model to {filepath}')
@@ -528,198 +527,11 @@ class ClosedLoopRandomTrainer(BaseTrainer):
                 # show model video every 50 epochs
                 self.model.eval()
                 if self.config['train']['eval']: self.evaluate(self.config['run_path'])
+                if self.config['train']['save']: super().save(self.config['run_path'], model_name=f'model_epoch{epoch+1}.pt')
                 self.model.train()
             
         pbar.close()
 
-
-class ClosedLoopPolicyTrainer(BaseTrainer):
-    """
-    Train a world model in closed loop, where actions are chosen based an asychronous
-    policy that is being trained concurrently.
-
-    Args:
-        dataset: Torch dataset object, from which test set is used during evaluation.
-        model: A world model class instance for training
-        config: Config dictionary with required params
-        device: Torch device object
-        policy: Asynchronous policy
-    """
-    def __init__(self, dataset, model, config, device, policy):
-        super().__init__(dataset, model, config, device, policy)
-        self.num_rollout_steps = config['closed_loop']['num_rollout_steps']
-        self.num_batches = config['closed_loop']['num_batches']
-        assert self.num_rollout_steps > self.batch_size, 'Steps per rollout must be > batch_size!'
-
-        # Initialize environment
-        disp = Display(visible=0, size=(480, 480))
-        disp.start()
-        env_name = config['train']['dataset'].split('_')[0]
-        self.env = gym.make(name_to_env[env_name], render_mode="rgb_array")
-        self.env_continuous = (env_to_aspace[env_name] == 'continuous')
-        self.past_length = config['trans']['past_length']
-
-        # Policy optimizer and loss (TODO)
-        self.policy_optimizer = torch.optim.Adam(
-            policy.parameters(), 
-            lr=config['closed_loop']['alpha'], 
-            weight_decay=config['closed_loop']['weight_decay']
-        )
-        self.policy_criterion = torch.nn.functional.mse_loss # TODO: Placeholder loss
-
-        # Initialize replay buffer
-        self.replay_buffer = ReplayBuffer(
-            self.in_image_shape, 
-            model.control_size, 
-            config['closed_loop']['buffer_capacity'],
-            device,
-            config
-        )
-        assert self.num_rollout_steps <= self.replay_buffer.capacity, 'Steps per rollout should be <= buffer_capacity!'
-
-    def collect_rollouts(self, epoch):
-        """
-        Collect observations and save them to replay buffer
-        """
-        # Initialize env and buffers
-        obs, _ = self.env.reset()
-        frame_buffer = []
-        act_buffer = []
-        idx = 0
-        while idx < self.num_rollout_steps:
-            # Render current frame
-            curr_img = process_image(self.env.render(), self.evaluator.dataset_name).permute(2, 0, 1)
-            if len(frame_buffer) == 0:
-                frame_buffer.append(curr_img)
-
-            # Sample and take action
-            # act = self.policy(curr_img.unsqueeze(0).to(self.device)).flatten()
-            act = torch.from_numpy(self.env.action_space.sample()).to(self.device)
-            act_buffer.append(act)
-            next_obs, rew, done, _, _ = self.env.step(act.cpu().detach().numpy())
-
-            # If done reset env, otherwise add sample to dataset
-            if done:
-                obs, _ = self.env.reset()
-                done = False
-                frame_buffer = []
-                act_buffer = []
-                continue
-            else:
-                # Slide frame obs history frame buffer to next image
-                if len(frame_buffer) == self.past_length + 1:
-                    frame_buffer.pop(0)
-                    act_buffer.pop(0)
-                next_image = process_image(self.env.render(), self.evaluator.dataset_name).permute(2, 0, 1)
-                frame_buffer.append(next_image)
-
-                # Add sample to replay buffer
-                if len(frame_buffer) == self.past_length + 1:
-                    self.replay_buffer.add(
-                        img = torch.stack(frame_buffer[0:self.past_length], dim=0),
-                        action = act_buffer[-1],
-                        reward = rew,
-                        next_img = frame_buffer[self.past_length],
-                        done = done
-                    )
-                    idx += 1
-
-    def train(self, epoch):
-        """
-        Gradient update for model and asychronous policy
-        """
-        # TODO: Where does policy update go, should be latent rollouts right?
-        # Sample batches
-        batches = []
-        for i in range(self.num_batches):
-            # Sampling
-            batches.append(self.replay_buffer.sample(self.batch_size))
-
-        # World model and policy update
-        total_model_loss, total_policy_loss = 0.0, 0.0
-        for i, batch in enumerate(batches):
-            # Unload batch
-            x, u, r, x_next, done  = batch
-            x, x_next, u = x.to(self.device), x_next.to(self.device), u.to(self.device)
-            x = x.reshape(x.shape[0], -1, *self.in_image_shape[1:])    # Stack obs history in channel dim
-            x_next = torch.hstack([x_next for i in range(self.model.past_length)]).to(self.device)
-
-            # Forward pass
-            train_return = self.model(x, x_next, u)
-            train_return['x'] = x
-            train_return['x_next'] = x_next
-
-            # Model loss and backprop
-            model_loss, loss_return = self.model_criterion(train_return, epoch)
-            self.plotter.log(loss_return)
-            self.model_optimizer.zero_grad()
-            model_loss.backward()
-            self.model_optimizer.step()
-
-            # TODO: Raise exception
-            if torch.isnan(model_loss):
-                print("NaN loss encountered, stopping training.")
-                break
-
-            total_model_loss += model_loss.item() * x.size(0)   # Aggregate total epoch loss
-
-            # Forward pass through policy
-            C = self.out_image_shape[0]
-            current_frame = x[:, -C:, :, :]  # Take only current frame
-            pred_action = self.policy(current_frame)
-
-            # Policy loss and backprop
-            policy_loss = self.policy_criterion(u, pred_action)
-            self.policy_optimizer.zero_grad()
-            policy_loss.backward()
-            self.policy_optimizer.step()
-
-            # TODO: Raise exception
-            if torch.isnan(policy_loss):
-                print("NaN loss encountered, stopping training.")
-                break
-
-            total_policy_loss += policy_loss.item() * x.size(0)
-
-        # Compute average model loss
-        avg_model_loss = total_model_loss / (self.batch_size*self.num_batches)
-
-        # # Policy update on same samples
-        # total_policy_loss = 0.0
-        # for i, batch in enumerate(batches):
-        #     x, u, r, x_next, done = batch
-        #     x = x.to(self.device).reshape(x.shape[0], -1, *self.in_image_shape[1:])
-
-        #     # Forward pass through policy
-        #     pred_action = self.policy(x)  
-
-        #     # Example policy loss: maximize expected reward in replay buffer
-        #     # You can replace this with SAC-style loss or any other RL objective
-        #     policy_loss = -torch.mean(pred_action)  
-
-        #     self.policy_optimizer.zero_grad()
-        #     policy_loss.backward()
-        #     self.policy_optimizer.step()
-
-        #     total_policy_loss += policy_loss.item() * x.size(0)
-
-        avg_policy_loss = total_policy_loss / (self.num_batches * self.batch_size)
-        return avg_model_loss, avg_policy_loss
-
-    def learn(self):
-        model_loss, policy_loss = 0.0, 0.0
-        pbar = tqdm(range(self.num_epochs), desc="Training")
-        for epoch in range(self.num_epochs):
-            self.collect_rollouts(epoch)
-            model_loss, policy_loss = self.train(epoch)
-
-            pbar.set_postfix({
-                'Epoch': epoch+1,
-                'Model Loss': f"{model_loss:.4f}",
-                'Policy Loss': f"{policy_loss:.4f}"})
-            pbar.update(1)
-            
-        pbar.close()
 
 class ClosedLoopInformativeTrainer(ClosedLoopRandomTrainer):
     """
