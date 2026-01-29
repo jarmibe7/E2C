024808b7fa7a213cbe8317c43a3679bcdd46862b
Ayush Gaggar <gaggarayush@gmail.com>
2026-01-29 13:07:28 -0600

    initial CEM configs for planning

--- src/eval_render_videos.py
+++ src/eval_render_videos.py
@@ -49,11 +49,18 @@ def load_trainer(config_name):
         output_uncertainty=(config['loss']['loss_type'] == 'uncertainty' or 'rssm' in config['loss']['loss_type'])
     )
 
+    config['closed_loop']['samples'] = 500
+    config['closed_loop']['plan_horizon'] = 8
     config['closed_loop']['sigma_init'] = 1.5
-    config['closed_loop']['sigma_min'] = 0.1
-    config['closed_loop']['elite_frac'] = 0.4
-    config['closed_loop']['iters'] = 4
-    config['closed_loop']['alpha'] = 0.4
+    config['closed_loop']['sigma_min'] = 0.05
+    config['closed_loop']['elite_frac'] = 0.1
+    config['closed_loop']['iters'] = 6
+    config['closed_loop']['alpha'] = 0.1
+
+    # config['closed_loop']['sigma_init'] = 1.5
+    # config['closed_loop']['sigma_min'] = 0.1
+    # config['closed_loop']['elite_frac'] = 0.4
+    # config['closed_loop']['iters'] = 4
     
     load_path = config.get('load_path', None)
     if load_path is None:
@@ -63,15 +70,15 @@ def load_trainer(config_name):
     # Load existing model to train from checkpoint
     load_path = load_path.split("model.pt")[0] if str(load_path).endswith('model.pt') else load_path
     model_path = load_path / 'model.pt' # can select model_200.pt if needed
-    print(f'Loading model from checkpoint: {model_path}\n')
+    print(f'Loading model from checkpoint: {model_path}')
     model.load_state_dict(torch.load(model_path))
     policy_type = config['closed_loop'].get('policy', None)
     if policy_type == 'random':
         trainer = ClosedLoopRandomTrainer(dataset, model, config, device)
     elif policy_type == 'informative':
-        trainer = ClosedLoopInformativeTrainer(dataset, model, config, device)
+        trainer = ClosedLoopInformativeTrainer(dataset, model, config, device, prints=False)
     elif policy_type == 'maxdyn':
-        trainer = ClosedLoopInformativeTrainer(dataset, model, config, device)
+        trainer = ClosedLoopInformativeTrainer(dataset, model, config, device, prints=False)
     else:
         raise ValueError(f"Unknown control policy type: {policy_type}")
     return trainer
@@ -80,19 +87,20 @@ def render_video(trainer: ClosedLoopRandomTrainer, max_steps=100, env_seed=0):
     trainer.curr_epoch = env_seed
     trainer.config['load_path'] = trainer.config['load_path'] if type(trainer.config['load_path']) == Path else Path(trainer.config['load_path'])
     saved_state = trainer.evaluator.render_video(trainer, trainer.config['load_path'], max_steps=max_steps, closed_loop=True, env_reset_seed=env_seed)
+    print(f"# of contacts: {torch.count_nonzero(saved_state[:, -1]).item()}\n")
     torch.save(saved_state, trainer.config['load_path'] / f'eval_states_{env_seed}.pt')
 
 if __name__ == "__main__":
-    for i in range(0, 1):
+    for i in range(1, 2):
         # for policy, objective in tqdm(zip(['random', 'eig', 'maxdyn'], ['random', 'pixel', 'dynamics'])):
         # for policy, objective in tqdm(zip(['maxdyn', 'eig'], ['pixel', 'dynamics'])):
         # for policy, objective in tqdm(zip(['random'], ['random'])):
         for policy, objective in zip(['eig'], ['pixel']):
         # for policy, objective in zip(['maxdyn', 'random'], ['dynamics', 'random']):
-            for env in ['drawer']: #, 'faucet', 'button']: # 'coffee', 'door' looks good...
-            # for env in ['door']: # [button, 'door', 'drawer', 'faucet']: #, 'button', 'coffee', 'door', 'drawer', 'faucet']:
+            # for env in ['drawer', 'faucet', 'button', 'coffee', 'door']: # looks good...
+            for env in ['faucet']: # [button, 'door', 'drawer', 'faucet']: #, 'button', 'coffee', 'door', 'drawer', 'faucet']:
                 config = "runs/{env}/{env}_{objective}_{i}/config.yaml".format(env=env, objective=objective, i=i)
-                print(f"Evaluating config: {config}")
+                # print(f"Evaluating config: {config}")
                 trainer = load_trainer(config)
-                for render_seed in range(0, 1):
+                for render_seed in range(12, 13):
                     render_video(trainer, max_steps=100, env_seed=render_seed)
